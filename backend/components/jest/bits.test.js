6797148b8cca76d98cd80d5e5acf25e63fd4e311
Vijivalovo <153453689+Vijivalovo@users.noreply.github.com>
2024-12-26 15:37:12 +0300

    Update bits.test.js

--- backend/components/jest/bits.test.js
+++ backend/components/jest/bits.test.js
@@ -1,9 +1,23 @@
 const request = require("supertest");
-const server = require('../../app'); // Путь к вашему файлу сервера
-describe("GET /bits", () =>{
-    it("should respond with 200 and a successful message when list all bits", async () => {
-        const response = await request(server).get("/bits/listBits");
+const { app, server } = require("../../app"); // Импорт сервера
+const db = require("../../components/database"); // Импорт базы данных
 
+// Подключаемся к базе данных перед запуском тестов
+beforeAll(async () => {
+    await db.connect();
+});
+
+// После всех тестов закрываем сервер и базу данных
+afterAll(async () => {
+    console.log("Closing server and database connection...");
+    server.close(); // Закрытие сервера
+    await db.close(); // Закрытие базы данных
+});
+
+// Тесты
+describe("GET /bits", () => {
+    it("should respond with 200 and a successful message when list all bits", async () => {
+        const response = await request(app).get("/bits/listBits");
         expect(response.statusCode).toBe(200);
     });
 });
